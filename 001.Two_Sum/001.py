3918d206951ee6f2c358ff51998885afc653f52d
Orion <orionbearduo@gmail.com>
2017-07-29 01:26:45 +0900

    Add files via upload

---
+++ 001.Two_Sum/001.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 29 01:18:54 2017
+
+@author: ichita
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
